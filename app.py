36e5952a4022efa5eb2917a920aca0e1f3489f6d
Mohammad H. Bahrampour <bahrampour@pm.me>
2020-01-24 19:19:36 +0330

    Add help page

---
+++ app.py
@@ -0,0 +1,11 @@
+# import libraries and module
+import functions
+
+# the main script
+def main():
+    
+    # show the help page
+    print( functions.help_page() )
+
+if __name__ == "__main__":
+    main()
---
+++ functions.py
@@ -0,0 +1,19 @@
+## help page
+
+# these content give some important options to use this app
+def help_page():
+    help_page = """
+    ---------------------------------
+    Wellcome to QA Password Manager !
+
+    You are using VERSION 1.0
+    To see the user manual type 'man'
+    To get out of the app type 'exit'
+
+    Intoduce some sign:
+    :: -> App questions or message.
+    !! -> Warning message.
+    ?? -> You can type what you want.
+    ---------------------------------
+    """
+    return help_page
